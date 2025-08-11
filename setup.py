229a303ffd86614a6eb643078ac5c371e58d6b6a
bansalakshat-12 <akshatbansal217@gmail.com>
2025-08-11 11:00:37 +0530

    after adding e

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> list[str]:
+    requirements_list = list[str] = []
+    return requirements_list
+
+setup(
+    name="sensor",
+    version = "0.0.1",
+    author= "Akshat",
+    author_email="akshatbansal217@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements()#["pymongo"]
+)
